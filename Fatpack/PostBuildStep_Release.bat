97add02be18cba303b7ead41ef5443b3e5ea91c2
Fatmike-GH <Fatmike_Github@gmx.net>
2025-06-17 12:58:04 +0200

    Renamed projects

--- Fatpack/PostBuildStep_Release.bat
+++ Fatpack/PostBuildStep_Release.bat
@@ -3,7 +3,7 @@ cd build
 cd x64
 cd Release
 
-START /WAIT ResourceAdder Loader_Console.exe Fatpack.exe 1000
+START /WAIT ResourceAdder Loader_Console_Resource.exe Fatpack.exe 1000
 powershell -command "Start-Sleep -Milliseconds 1000"
-START /WAIT ResourceAdder Loader_Windows.exe Fatpack.exe 1001
+START /WAIT ResourceAdder Loader_Windows_Resource.exe Fatpack.exe 1001
 powershell -command "Start-Sleep -Milliseconds 1000"
